b27fd66edc72ff025bb5a18879f2872456f85a38
Divya <divyadhakshin66@gmail.com>
2025-08-13 10:10:17 +0530

    Modification including another function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-from src.main import add
+from src.main import add,sub
 def test_add_function():
     assert add(1,2) == 3
     assert add(0,0) == 0
     assert add(5,5) == 10
+def test_sub_function():
+    assert sub(5,2) == 3
+    assert sub(0,0) == 0
+    assert sub(15,5) == 10
